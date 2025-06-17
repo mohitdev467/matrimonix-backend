22a5596c13831a8d64a5b169cd538085a5378f26
mohitdev467 <141304654+mohitdev467@users.noreply.github.com>
2025-06-17 18:39:02 +0530

    message added

--- src/controllers/DeleteRequestController/DeleteRequestController.js
+++ src/controllers/DeleteRequestController/DeleteRequestController.js
@@ -4,9 +4,9 @@ exports.createDeleteRequest = async (req, res) => {
   try {
     const newRequest = new DeleteRequestSchema(req.body);
     await newRequest.save();
-    res.status(201).json({ success: true, data: newRequest });
+    res.status(201).json({ success: true, message:"Delete request created successfully", data: newRequest });
   } catch (error) {
-    res.status(500).json({ success: false, error });
+    res.status(500).json({ success: false, message:"Internal Server Error", error });
   }
 };
 
@@ -59,7 +59,7 @@ exports.deleteRequest = async (req, res) => {
   try {
     const deleted = await DeleteRequestSchema.findByIdAndDelete(req.params.id);
     if (!deleted) return res.status(404).json({ success: false, error: "User Not found" });
-    res.status(200).json({ success: true, message: "Deleted request successfully" });
+    res.status(200).json({ success: true, message: "Delete request successfully" });
   } catch (error) {
     res.status(500).json({ success: false, error });
   }
